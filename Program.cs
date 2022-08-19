c4b3d3679c04901c6d2f7aecfd3bf665672eaadc
unknown <gritsunov85@gmail.com>
2022-08-19 14:21:58 +0600

    End

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+string result = "";
+
+while (i < number)
+{
+
+  i++;
+  if (number == i)
+  {
+    result = result + (i * i * i);
+  }
+  else
+  {
+    result = result + (i * i * i) + ',';
+  }
+}
+
+Console.WriteLine(result);
